9f912decea81dec06b5924e0c0b2f011586325fa
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-14 17:09:01 +0200

    feat(bonus): disable function when game ends, base excercise plus some bonus complete

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -5,12 +5,17 @@ const button = document.getElementById("play");
 const container = document.getElementById("contenitore");
 
 // creo costante selettore difficoltà
-const inputLevel = document.getElementById("difficolta")
+const inputLevel = document.getElementById("difficolta");
+
+
 
 // evento click pulsante gioca
 button.addEventListener("click",
 
     function() {
+
+        // variabile da usare successivamente per disattivare il click al termine della partita
+        let enable = true;
 
         // svuotiamo il container
         container.innerHTML= " ";
@@ -26,11 +31,11 @@ button.addEventListener("click",
 
         // if else per cambiare la classe
         if (level === 100) {
-            classDiff = "box-100"
+            classDiff = "box-100";
         } else if (level === 81) {
-            classDiff = "box-81"
+            classDiff = "box-81";
         } else {
-            classDiff = "box-49"
+            classDiff = "box-49";
         }
 
         // aggiungo array con numeri bombe | argomenti: numero massimo bombe, numero random minimo, e numero random massimo che dipende dalla difficoltà
@@ -50,7 +55,6 @@ button.addEventListener("click",
 
         // creo array di numeri già clickati mi serve anche da reset
         const clickati = [];
-        console.log("reset clickati" + clickati);
 
         // creo ciclo per far inserire elementi nel container
         for (let i=1; i <=level; i++) {
@@ -70,44 +74,48 @@ button.addEventListener("click",
             // inserisco il testo nel div col numero
             boxNum.innerHTML=(i);
 
+            
+            
             // associo al boxdiv l'evento di click che determina gli esiti della partita
             boxDiv.addEventListener("click",
                 function () {
 
                     // se il box clickato non è nella lista delle bombe e non è già stato clickato
-                    if (!(bombs.includes(i)) && !(clickati.includes(i))) {
+                    if (!(bombs.includes(i)) && !(clickati.includes(i)) && enable === true) {
                         // aggiungo la classe safe che colora di azzurro
                         boxDiv.classList.add("safe");
-                        console.log("La cella cliccata è la numero: " + i);
                         // aumento il punteggio
                         points++;
                         // aggiungo a clickati
-                        clickati.push(i);
-                        console.log("aggiunto a clickati" + clickati);
-                        
-                    } else if (bombs.includes(i)) {
+                        clickati.push(i);         
+
+                    } else if (bombs.includes(i) && enable === true) {
                         // altrimenti aggiungo al classe bomb che colora di rosso
-                        boxDiv.classList.add("bomb")
-                        console.log("La cella cliccata è una bomba: " + i);
+                        boxDiv.classList.add("bomb");
                         // mostro il div con l'esito finale e il punteggio
                         endGame.classList.add("esito-finale-active");
-                        endGame.innerHTML=`Oh no, hai colpito una bomba! Il tuo punteggio è di: ${points}`
+                        endGame.innerHTML=`Oh no, hai colpito una bomba! Il tuo punteggio è di: ${points}`;
+                        // variabile per disattivare altri click
+                        enable = false;
                     }
 
                     // aggiungo sempre nel click, un caso rarissimo di utente che vince evitando tutte le bombe
-                    if (points === (level - bombs.length)) {
+                    if (points === (level - bombs.length) && enable === true) {
                         endGame.classList.add("esito-finale-active");
-                        endGame.innerHTML=`E' incredibile, hai evitato tutte le bombe, sei nell'olimpo dei migliori. Hai totalizzato il punteggio massimo di: ${points}`
-
+                        endGame.innerHTML=`E' incredibile, hai evitato tutte le bombe, sei nell'olimpo dei migliori. Hai totalizzato il punteggio massimo di: ${points}`;
+                        // variabile per disattivare altri click
+                        enable = false;
                     }
 
                 }
             )
 
         }
-
+        
         
     }
-
+    
 
 )
+
+
